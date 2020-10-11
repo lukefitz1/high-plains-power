d921843252248510a637b22354a53742ce245afa
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2020-10-11 15:48:15 -0600

    separating public from private routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations]
+  devise_for :users#, skip: [:registrations]
 
   root to: 'static#homepage'
   get '/products', to: 'products#index'
-  get 'products/:id', to: 'products#show'
+  get '/products/:id', to: 'products#show', as: 'product'
 
   authenticate :user do
-    resources :products
+
+    resource :admin do
+      resources :products
+    end
   end
 end
